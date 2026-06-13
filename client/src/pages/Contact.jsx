b0b8c9e348a98ea943e0fc16c7dd8a39d9d154d6
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-14 00:30:14 +0530

    feat: initialize React project with Vite, add basic pages and assets
    - Added React and Vite setup with main entry point and index.css
    - Created About, Contact, Home, and Products pages
    - Implemented Products page with API integration for fetching products
    - Added SVG assets for React and Vite logos
    - Included loading state for product fetching
    - Configured Tailwind CSS for styling

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+       <div>Contact</div>
+    </>
+  )
+}
+
+export default Contact;
+
+
+
+//  http://makeup-api.herokuapp.com/api/v
