f2df33943513a023f609c2e13213f83e7255f399
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-14 14:44:00 +0530

    feat: add Tailwind CSS integration, create Navbar and Footer components, and add Login and Register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
