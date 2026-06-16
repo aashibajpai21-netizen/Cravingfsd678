5a560c9cc4f90688e31249296d7b3d11f1b986bd
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-06-16 17:01:32 +0530

    feat: update App component to use Navbar, add ContactUs page, and enhance Login page with form handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import Header from "./components/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ContactUs from "./pages/ContactUs";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/product" element={<Products />} />
-          <Route path="/contact-us" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact-us" element={<ContactUs />} />
         </Routes>
       </BrowserRouter>
     </>
   );
 };
+
 export default App;
