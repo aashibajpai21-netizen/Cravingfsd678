8f5509bc28f64bd77e1358b9fdbfdf40b67bb9f9
aashibajpai21-netizen <aashibajpai21@gmail.com>
2026-07-03 20:51:55 +0530

    feat: update user dashboard layout and add sidebar navigation; enhance authentication middleware with JWT verification

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+// 
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,12 +1,21 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+
 export const AuthProtect = async (req, res, next) => {
   try {
     //Controller Logic
+    const token = req.cookies,Oreo;
 
-    next();
-  } catch (error) {
-    console.log(error.message);
-    const error1 = new Error("Unknown Error At Middleware");
-    error.statusCode = 500;
-    next(error);
-  }
+    console.log("Token from Middleware :", token);
+    const decode = await jwt.verify(token, process.env.JWT_SECRET);
+    if(!decode)
+    {
+      const error = new Error("Session Expired");
+      error.statusCode = 401;
+      next(error);
+    }
+    const verifiedUser = await (User.findById:decode._id);
+
+  
 };
